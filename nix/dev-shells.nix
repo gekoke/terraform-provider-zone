1d648de3da5f6664946e95ebc5b6f138a3fea0ae
Gregor Grigorjan <gregor.grigorjan@gamesglobal.com>
2025-03-14 12:34:08 +0200

    refactor(nix): devShells: remove `name` attribute
    Not needed apparently!

--- nix/dev-shells.nix
+++ nix/dev-shells.nix
@@ -10,8 +10,6 @@
     {
       devShells = {
         default = pkgs.mkShellNoCC {
-          name = "terraform-provider-zone-shell";
-
           packages = [
             pkgs.golangci-lint
             pkgs.opentofu
